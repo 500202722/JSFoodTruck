dd14d1b186c64490ffb01bd8bcf033015f9014b1
Evan Keane <evankeane0@icloud.com>
2025-04-22 16:20:26 -0500

    Added Starter Files

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+const root = path.join(__dirname, 'public')
+const app = express()
+
+const port = 3000
+
+app.use(express.json())
+app.use(express.static('public'))
+
+app.use('/api/v1/menu/', require('./routes/api/v1/foodtruck'))
+app.use('/api/v1/events/', require('./routes/api/v1/foodtruck'))
+app.use(require('./routes/static'))
+
+app.listen(port, () => console.log(`http://localhost:${port}`))
---
+++ routes/api/v1/foodtruck.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+const { getCollection, ObjectId } = require('../../../dbconnect')
+
+let collection = null
+
+
+router.get('/', async (request, response) => {
+
+})
+
+module.exports = router
